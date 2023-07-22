ed2eb3263f47fbc60966163a3a115ba06f6afbef
qwerq123456 <qwerq123456@naver.com>
2023-07-23 00:05:06 +0900

    feat: apply style

--- src/App.js
+++ src/App.js
@@ -20,3 +20,53 @@ function App() {
 }
 
 export default App;
+
+
+
+// // 
+// return (
+//     <div>
+//         <div style={{ width: 100, height: 100, border: "1px solid black", margin: 1, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around" }}>
+//             <div>
+//                 {name}
+//             </div>
+//             <div>
+//                 {picture}
+//             </div>
+//             <div>
+//                 {insta}
+//             </div>
+//         </div>
+//         <div style={{ width: 100, height: 100, border: "1px solid black", margin: 1, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around" }}>
+//             <div>
+//                 {name}
+//             </div>
+//             <div>
+//                 {picture}
+//             </div>
+//             <div>
+//                 {insta}
+//             </div>
+//         </div>        <div style={{ width: 100, height: 100, border: "1px solid black", margin: 1, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around" }}>
+//             <div>
+//                 {name}
+//             </div>
+//             <div>
+//                 {picture}
+//             </div>
+//             <div>
+//                 {insta}
+//             </div>
+//         </div>        <div style={{ width: 100, height: 100, border: "1px solid black", margin: 1, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around" }}>
+//             <div>
+//                 {name}
+//             </div>
+//             <div>
+//                 {picture}
+//             </div>
+//             <div>
+//                 {insta}
+//             </div>
+//         </div>
+//     </div>
+// )
